4546b88dfdfd283d1f881e55b700fd38baf26386
Ali Khalili <ali.khalili@gmail.com>
2017-01-27 15:58:06 -0800

    feat: Add trackvehicles.py for implementing the project

---
+++ trackvehicles.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 15:35:26 2017
+
+@author: Ali.Khalili
+"""
+
+from helperfunctions import *
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+if __name__ =='__main__':
+    main()
